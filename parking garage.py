fc5582af59ae96e29b56040053e2febbf77ef0ec
Sam-Checkers <147878291+Sam-Checkers@users.noreply.github.com>
2023-12-12 15:06:37 -0500

    Update parking garage.py

--- parking garage.py
+++ parking garage.py
@@ -1,15 +1,16 @@
-class parking_garage():
+class parking_garage:
     
-    tickets = 10
-    parkingSpaces = 10
-    payment = []
-    currentTicket = {'ticket status: paid'}
-    enter =[]
-    
+    def __init__(self):
+        self.tickets = 10
+        self.parkingSpaces = 10
+        self.payment = []
+        self.currentTicket = {'ticket status: paid'}
+        self.enter = []
+
     def takeTicket(self):
-        enter = input('take ticket? Y/N')
+        enter = input('Take ticket? Y/N: ')
         self.enter.append(enter)
-        if enter.lower() == 'y' and self.tickets !=0:
+        if enter.lower() == 'y' and self.tickets != 0:
             self.tickets = self.tickets - 1
             self.parkingSpaces = self.parkingSpaces - 1
             self.currentTicket = False
@@ -21,10 +22,9 @@ class parking_garage():
             print('Then why are you here?')
             self.currentTicket = False
 
-    
     def payForParking(self):
         if self.enter == ['y'] or self.enter == ['Y']:
-            fee = input('To pay now, insert $5.00 Y/N')
+            fee = input('To pay now, insert $5.00 Y/N: ')
             if fee.lower() == 'y':
                 self.payment.append(5)
                 self.currentTicket = True
@@ -33,15 +33,15 @@ class parking_garage():
                 self.currentTicket = False
         else:
             print('No ticket, no entry')
-            
+
     def leaveGarage(self):
         if self.enter == ['y'] or self.enter == ['Y']:
             if self.currentTicket == True:
                 print('Thank you, have a nice day!')
-                self.tickets = self.tickets +1
+                self.tickets = self.tickets + 1
                 self.parkingSpaces = self.parkingSpaces + 1
             if self.currentTicket == False:
-                fee = input('insert $5.00 Y/N')
+                fee = input('Insert $5.00 Y/N')
                 if fee.lower() == 'y':
                     self.payment.append(5)
                     self.currentTicket = True
